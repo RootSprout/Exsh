495124843c7c7a677693333038c9b4939656bb53
Meerthika <meerthikasrrajendrans@gmail.com>
2025-09-07 14:48:45 +0530

    [Rename] strtok

--- src/lexer.c
+++ src/lexer.c
@@ -1,21 +1,18 @@
-#include "lexer.h"
+#include "../include/lexer.h"
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 char** tokenize(char* input, int* ntokens){
     char** tokens = malloc(64 * sizeof(char*));
     int pos = 0;
-    char* token = strok(input, " \t\n");
+    char* token = strtok(input, " \t\n");
     while(token!= NULL){
         tokens[pos++] = token;
-        token = strok(NULL, "\t\n");
+        token = strtok(NULL, " \t\n");
     }
     tokens[pos] = NULL;
     *ntokens = pos;
-    printf("Tokens:\n");
-    for (int t = 0; t < ntokens; t++) {
-        printf("  [%d] %s\n", t, tokens[t]);
-    }
 
     return tokens;
 }

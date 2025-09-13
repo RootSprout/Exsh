e8d04c5a5f9a63ab718774321772beaf70184d3f
adr420 <aditya000000420@gmail.com>
2025-09-13 11:08:34 +0530

    Added optional libreadline support, provides cursor control and history functionality to the prompt.

--- src/main.c
+++ src/main.c
@@ -1,23 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "include/lexer.h"
 #include "include/parser.h"
 #include "include/executor.h"
 
+#ifdef USE_LIBREADLINE
+#include <readline/readline.h>
+#include <readline/history.h>
+#endif
+
 int main() {
-    char line[1024];
+	char line[1024];
+	const char *prompt = "Exsh$ ";
 
-    while (1) {
-        printf("Exsh$ ");
-        if (!fgets(line, sizeof(line), stdin)) break;
+	while (1) {
+#ifdef USE_LIBREADLINE
+		strncpy(line, readline(prompt), 1024);
+		add_history(line);
+#else
+		printf(prompt);
+		if (!fgets(line, sizeof(line), stdin)) break;
+#endif
+		int ntokens;
+		char **tokens = tokenize(line, &ntokens);
+		if (ntokens == 0) continue;
 
-        int ntokens;
-        char **tokens = tokenize(line, &ntokens);
-        if (ntokens == 0) continue;
+		Command *cmds = parse_tokens(tokens, ntokens);
+		execute_commands(cmds);
+		free_cmd(cmds);
+	}
 
-        Command *cmds = parse_tokens(tokens, ntokens);
-        execute_commands(cmds);
-        free_cmd(cmds);
-    }
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
